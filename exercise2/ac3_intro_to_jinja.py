7d636781dac8a2c6a0005c275f1b47ce850a7348
Claudia de Luna <cldeluna@yahoo.com>
2025-04-29 13:46:08 -0700

    Reordered

--- exercise2/ac3_intro_to_jinja.py
+++ exercise2/ac3_intro_to_jinja.py
@@ -14,18 +14,27 @@ Original file is located at
 """jinja2 already available in colab environment"""
 
 tmp_str = "I am in {{ city }}"
+tmp_str3 = "I am in {{ payload['city'] }}"
 
 import jinja2 as j2
 
 t = j2.Template(tmp_str)
 
 result = t.render(city="Denver")
-
 print(result)
 
 mydict = {"city": "Los Angeles"}
 
 result2 = t.render(mydict)
-
 print(result2)
+
+t = j2.Template(tmp_str)
+result3 = t.render(payload=mydict)
+print(result3)
+
+t3 = j2.Template(tmp_str3)
+result4 = t3.render(payload=mydict)
+print(result4)
+
+
 
